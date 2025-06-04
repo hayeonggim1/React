68dbb96b1afcac0ac3d7c1d75eda94b6cf5c739f
hayeonggim1 <hayeonggim1@gmail.com>
2025-06-04 09:43:56 +0900

    0604-1

--- workspace/ch01-buildup/todolist/09/src/reaction.js
+++ workspace/ch01-buildup/todolist/09/src/reaction.js
@@ -17,13 +17,18 @@ const reaction = {
       }
     }
     // 자식 노드 추가
-    for(let child of children){
-      if(typeof child === 'string' || typeof child === 'number'){
+    for (let child of children) {
+      if (typeof child === "string" || typeof child === "number") {
         child = document.createTextNode(child);
-      }else if(typeof child === 'function'){
+      } else if (typeof child === "function") {
         child = child();
       }
-      elem.appendChild(child);
+
+      if(Array.isArray(child)){
+        child.forEach(c => elem.appendChild(c))
+      }else{
+        elem.appendChild(child);
+      }
     }
     // 요소 노드 반환
     return elem;
@@ -36,7 +41,7 @@ const reaction = {
     return _root = {
       // 루트 노드 하위에 지정한 함수를 실행해서 받은 컴포넌트를 렌더링 한다.
       render(appFn){
-        _appComponent = _appComponent ?? appFn;
+        _appComponent = _appComponent || appFn;
         rootNode.firstChild?.remove();
         rootNode.appendChild(_appComponent());
       }
@@ -46,9 +51,7 @@ const reaction = {
   // 상태값(데이터) 관리
   useState: (initialValue) => {
     // 최초 호출되었을 경우에만 초기값을 지정하고 이후에 다시 호출되는 경우에는 이전 값을 유지한다.
-    // ??: null 병합 연산자
-    // 왼쪽 피연사가 null, undefined일 때 오른쪽 값을 사용
-    _stateValue = _stateValue ?? initialValue;
+    _stateValue = _stateValue || initialValue;
 
     // 상태값을 수정한다.
     function setValue(newValue){
